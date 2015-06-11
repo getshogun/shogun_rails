f63e3be94472fa0422e8ef0530bd83ce0e3f84c8
Finbarr Taylor <finbarrtaylor@gmail.com>
2015-06-11 13:05:59 -0700

    Bumped version and fixed layout bug.

--- app/controllers/shogun/editor_controller.rb
+++ app/controllers/shogun/editor_controller.rb
@@ -3,5 +3,6 @@ class Shogun::EditorController < ApplicationController
     d = Digest::SHA256.hexdigest "#{Shogun.site_id}-#{Shogun.secret_token}"
     return head :unauthorized unless params[:d] == d
     response.headers.except! 'X-Frame-Options'
+    render layout: Shogun.layout
   end
 end
--- lib/shogun/version.rb
+++ lib/shogun/version.rb
@@ -1,3 +1,3 @@
 module Shogun
-  VERSION = "0.1.7"
+  VERSION = "0.1.8"
 end
