5a4066c1f7a15b99c703ba75ce9dad714b22a59b
Finbarr Taylor <finbarrtaylor@gmail.com>
2015-06-22 17:25:53 -0700

    Shogun lang.

--- app/controllers/shogun/previews_controller.rb
+++ app/controllers/shogun/previews_controller.rb
@@ -3,7 +3,12 @@ require "http"
 class Shogun::PreviewsController < ApplicationController
   def show
     not_found! unless Shogun.secret_token.present?
-    r = HTTP.headers("X-Secret-Token" => Shogun.secret_token).get("#{Shogun.preview_url}/previews/#{params[:uuid].to_s}")
+    sl = params[:shogun_lang].to_s
+    url = "#{Shogun.preview_url}/previews/#{params[:uuid].to_s}"
+    if sl.present?
+      url += "?lang=#{sl}"
+    end
+    r = HTTP.headers("X-Secret-Token" => Shogun.secret_token).get(url)
     not_found! unless r.status.code == 200
     @html = r.to_s
     render layout: Shogun.layout
