7540841560aabaa473ff04fa99a09e1958dbb5e5
Finbarr Taylor <finbarrtaylor@gmail.com>
2015-08-12 16:52:16 -0700

    Allow inner iframe to be loaded.

--- app/controllers/shogun/inner_controller.rb
+++ app/controllers/shogun/inner_controller.rb
@@ -2,6 +2,7 @@ class Shogun::InnerController < ApplicationController
   def show
     d = Digest::SHA256.hexdigest "#{Shogun.site_id}-#{Shogun.secret_token}"
     return head :unauthorized unless params[:d] == d
+    response.headers.except! 'X-Frame-Options'
     render layout: Shogun.layout
   end
 end
